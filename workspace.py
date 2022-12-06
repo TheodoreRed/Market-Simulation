319fbfa7458b6943c8afbed4b9b2c0b9ae177922
Theodore Redlon <redlontheo@gmail.com>
2022-12-06 07:54:02 -0500

    More changes

--- workspace.py
+++ workspace.py
@@ -13,10 +13,8 @@ class Buyer:
     def new_position(self, new_asset, quantity):
         isNew = True
         for asset in self.all_assets:
-            if new_asset.name == asset.name:
+            if new_asset.name == asset.name and type(new_asset) == type(asset):
                 isNew = False
-                if type(new_asset) != type(asset):
-                    isNew = True
 
         if isNew:
             total_equity = new_asset.current_price * quantity
@@ -35,17 +33,53 @@ class Buyer:
 
     def add_to_position(self, type_asset, name, quantity):
         for asset in self.all_assets:
+            if self.uninvested >= asset.current_price * quantity:
+                if type(asset) == type_asset:
+                    if asset.name == name:
+                        self.uninvested -= asset.current_price * quantity
+                        self.invested += asset.current_price * quantity
+                        asset.quantity += quantity
+            else:
+                print("not enough money")
+
+    def close_position(self, asset):
+        val_of_asset = asset.current_price * asset.quantity
+        self.uninvested += val_of_asset
+        self.invested -= val_of_asset
+        self.all_assets.remove(asset)
+
+    def subtract_from_position(self, type_asset, name, quantity):
+        copy_all_assets = self.all_assets
+        for asset in self.all_assets:
+
+            # type and name create a unique key
             if type(asset) == type_asset:
                 if asset.name == name:
-                    self.uninvested -= asset.current_price * quantity
-                    self.invested += asset.current_price * quantity
-                    asset.quantity += quantity
+                    if quantity >= asset.quantity:
+                        self.close_position(asset)
+                        break
+                    else:
+                        self.uninvested += asset.current_price * quantity
+                        self.invested -= asset.current_price * quantity
+                        asset.quantity -= quantity
 
-    def subtract_from_position():
-        pass
-
-    def close_position():
-        pass
+    def display(self):
+        print("{}'s Current Portfolio".format(self.name))
+        for asset in self.all_assets:
+            if type(asset) == Stock:
+                the_type = "Stock"
+            elif type(asset) == Bond:
+                the_type = "Bond"
+            print(
+                "{} {} : {} of this asset at a price of ${} dollars each".format(
+                    the_type, asset.name, asset.quantity, asset.current_price
+                )
+            )
+        print(
+            "In cash still: ${} Total invested: ${} Buyer name: {}\n".format(
+                self.uninvested, self.invested, self.name
+            )
+        )
 
 
 class Asset:
@@ -54,41 +88,52 @@ class Asset:
         self.current_price = current_price
         self.quantity = 0
 
+        def val_of_asset(self):
+            return self.current_price * self.quantity
+
 
 class Stock(Asset):
-    def __init__(self, name, current_price):
+    def __init__(self, name, current_price, is_large_company):
         Asset.__init__(self, name, current_price)
+        self.is_large_company = is_large_company
 
 
 class Bond(Asset):
-    def __init__(self, name, current_price, rate):
+    def __init__(self, name, current_price):
+        RECENT_AVG_RATE = 0.0689
         Asset.__init__(self, name, current_price)
-        self.rate = rate
+        self.rate = RECENT_AVG_RATE
 
 
-RECENT_AVG_RATE = 0.0689
 ted = Buyer(10000, "Ted")
-ted.new_position(Stock("MSFT", 255), 3)
-ted.new_position(Stock("AAPL", 149), 1)
-ted.new_position(Bond("USA", 50, RECENT_AVG_RATE), 12)
-ted.new_position(Stock("USA", 6), 10)
+ted.new_position(Stock("MSFT", 255, True), 3)
+ted.new_position(Stock("AAPL", 149, True), 15)
+ted.new_position(Bond("USA", 50), 12)
+ted.new_position(Stock("USA", 6, False), 100)
 
 ted.add_to_position(Bond, "USA", 11)
-ted.add_to_position(Stock, "USA", 190)
+ted.add_to_position(Stock, "USA", 191)
+ted.display()
+ted.subtract_from_position(Stock, "AAPL", 1)
+
+ted.display()
+ted.add_to_position(Stock, "MSFT", 4)
+ted.display()
 
 
-for asset in ted.all_assets:
-    if type(asset) == Stock:
-        the_type = "Stock"
-    elif type(asset) == Bond:
-        the_type = "Bond"
+def display(self):
+    for asset in self.all_assets:
+        if type(asset) == Stock:
+            the_type = "Stock"
+        elif type(asset) == Bond:
+            the_type = "Bond"
+        print(
+            "{} {} : There are {} at a price of ${} dollars each".format(
+                the_type, asset.name, asset.quantity, asset.current_price
+            )
+        )
     print(
-        "{} {} : There are {} at a price of ${} dollars each".format(
-            the_type, asset.name, asset.quantity, asset.current_price
+        "In cash still: ${} Total invested: ${} Buyer name: {}".format(
+            self.uninvested, self.invested, self.name
         )
     )
-print(
-    "In cash still: ${} Total invested: ${} Buyer name: {}".format(
-        ted.uninvested, ted.invested, ted.name
-    )
-)
